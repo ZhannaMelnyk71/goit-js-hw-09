6c0fa9cc668af02a4c4094c7c6b194258cd2e561
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-03-23 20:18:21 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn)
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn)
+const bodyEl = document.querySelector('body')
+console.log(bodyEl)
+
+function startChangeColor(event) {
+    
+timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  
+
+};
+
+function stopChangeColor(event){
+clearInterval(timerId);
+};
+
+startBtn.addEventListener("click", startChangeColor)
+stopBtn.addEventListener("click", stopChangeColor)
