08f0c516434cf004eddc750fd51da7bf0d496e83
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-03-23 21:44:48 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,4 @@
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
 
 const startBtn = document.querySelector('button[data-start]');
 console.log(startBtn)
@@ -10,18 +7,40 @@ console.log(stopBtn)
 const bodyEl = document.querySelector('body')
 console.log(bodyEl)
 
+let timerId = null;
+
 function startChangeColor(event) {
-    
+  if (timerId) {
+    return;
+  }
 timerId = setInterval(() => {
     bodyEl.style.backgroundColor = getRandomHexColor();
-  }, 1000);
+}, 1000);
+  isStartBtnActive()
   
-
 };
 
 function stopChangeColor(event){
-clearInterval(timerId);
+  timerId = clearInterval(timerId);
+  
+  isStopBtnActive()
 };
+
+function isStartBtnActive() {
+  if (startChangeColor) { startBtn.disabled = true;  
+  }
+  stopBtn.disabled = false;
+}
+
+function isStopBtnActive() {
+  if (stopChangeColor) { stopBtn.disabled = true;  
+  }
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
 startBtn.addEventListener("click", startChangeColor)
 stopBtn.addEventListener("click", stopChangeColor)
