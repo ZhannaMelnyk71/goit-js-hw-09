a3234487b8ff8392952bd5049a368e6da5ab07b9
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-03-30 23:18:57 +0200

    task-3-finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,14 +3,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('form');
-const inputDelay = document.getElementsByName('delay')
-const inputStep = document.getElementsByName('step')
-const inputAmount = document.getElementsByName('amount')
-const BtnEl = document.querySelector('button')
 
-// BtnEl.setAttribute('disabled', '');
-
-BtnEl.addEventListener('click', handleFormSubmit);
+form.addEventListener('submit', handleFormSubmit);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -28,34 +22,18 @@ function createPromise(position, delay) {
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let delay = 0;
-  let step = inputStep[0].value;
-  let amount = inputAmount[0].value;
+  const {delay, amount, step} = evt.target.elements
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
 
-  for (let i = 1; i <= amount; i++){
-    delay = inputDelay[0].value + step * (i - 1);
-    createPromise(i, delay)
+  for (let i = 0; i <= amountValue; i++){
+    createPromise(i, delayValue)
       .then(({position, delay}) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
       .catch(({position, delay}) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
   }
-  // form.reset();
+  delayValue += stepValue;
 }
 
 
-// function isInputFulfilled(evt) {
-//   evt.preventDefault();
-//   const {
-//     elements: { delay, step, amount }
-//   } = evt.currentTarget;
-//   if (Number(delay.value) === "" || Number(step.value) === "" || Number(amount.value) === "") {
-//     Notiflix.Notify.failure("Please fill in all the fields!");
-//     return;
-//   }
-//   if (Number(delay.value) <= 0 || Number(step.value) <= 0 || Number(amount.value) <= 0) {
-//     Notiflix.Notify.failure("The value can't be negative or zero");
-//     return;
-//   }
-//   BtnEl.removeAttribute('disabled')
-
-// }
 
